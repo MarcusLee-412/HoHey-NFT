a3aeef8f369cd26df22ce4ef99ecb4eb8b27cdc8
mortem <lampakyu@gmail.com>
2021-06-11 15:58:05 +0800

    fixed errors?

--- contracts/Test.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity >=0.4.22 <0.9.0;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
-import "@openzeppelin/contracts/drafts/Counters.sol";
-
-contract Films is ERC721Full {
-    constructor() ERC721("Film", "FILM") {}
-}
